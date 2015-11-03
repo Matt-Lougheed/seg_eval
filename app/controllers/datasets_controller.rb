74283589e8a4340325776e2a1ae2d7b894bbd50e
Matt Lougheed <lougheed@cs.queensu.ca>
2015-11-03 12:30:48 -0500

    Reorganized dataset creation code, added automatic dimension detection for the mha files, and removed unnecessary form parameters from new dataset form

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -23,49 +23,27 @@ class DatasetsController < ApplicationController
             @dataset.ground_truth = dataset_params[:ground_truth].original_filename.to_s
         end
 
-        # Save the dataset, then we must write the files
+        # Save the dataset, then we must write the files and update the dataset
         if @dataset.save
             # Write the dataset to file
-            uploaded_file = dataset_params[:image_sequence]
-            dir_path = Rails.root.join('public','uploads','dataset',current_user.id.to_s,@dataset.id.to_s)
-            FileUtils.mkdir_p(dir_path) unless File.directory?(dir_path)
-            file_path = Rails.root.join(dir_path, @dataset.image_sequence)
-            File.open(file_path, 'wb') do |file|
-                file.write(uploaded_file.read)
-            end
+            @dataset.write_sequence_to_file(dataset_params[:image_sequence], @dataset.image_sequence)
 
             # Write the ground truth to file
-            ground_truth_file = dataset_params[:ground_truth]
-            ground_truth_path = Rails.root.join(dir_path, @dataset.ground_truth)
-            File.open(ground_truth_path, 'wb') do |file|
-                file.write(ground_truth_file.read)
-            end
+            @dataset.write_sequence_to_file(dataset_params[:ground_truth], @dataset.ground_truth)
 
+            # Find the dimensions for the sequence
+            @dataset.find_image_dimensions
+            
             # Create frame and thumbnail for .mha file
-            if (File.extname(file_path) == ".mha")
-                result = system(Rails.root.join('scripts','mha_to_png','bin',"MhaToPng #{file_path} 1").to_s)
-                base_name = File.basename(file_path, ".mha")
-                image = MiniMagick::Image.open("#{dir_path}/#{base_name}_frame.png")
-                image.resize "200x200"
-                image.format "png"
-                image.write "#{dir_path}/#{base_name}_thumbnail.png"
-                File.chmod(0644,"#{dir_path}/#{base_name}_thumbnail.png")
-                @dataset.thumbnail = "#{base_name}_thumbnail.png"
-                @dataset.frame = "#{base_name}_frame.png"                
-            end
+            @dataset.create_frame_and_thumbnail
 
             # Zip the files together as a package for users to download
-            require 'zip'
-            zip_files = [ground_truth_file.original_filename, uploaded_file.original_filename]
-            zip_filename = Rails.root.join(dir_path, "Dataset_#{@dataset.id}.zip")
-            Zip::File.open(zip_filename, Zip::File::CREATE) do |zipfile|
-                zip_files.each do |file|
-                    zipfile.add(file, "#{dir_path}/#{file}")
-                end
-            end
+            @dataset.create_zip
 
             # Update the dataset with the new paths for thumbnail and frame
             @dataset.save
+
+            # Redirect to newly created dataset and display success notification
             flash[:success] = "Success: new dataset created!"
             redirect_to @dataset
         else
@@ -75,7 +53,7 @@ class DatasetsController < ApplicationController
 
     def download
         dataset = Dataset.find(params[:dataset_id])
-        send_file "#{Rails.root}/public/uploads/dataset/#{dataset.user_id}/#{dataset.id}/Dataset_#{dataset.id}.zip"
+        send_file "#{Rails.root}/public/uploads/dataset/#{dataset.user_id}/#{dataset.id}/#{dataset.name}.zip"
         Dataset.increment_counter(:download_num, dataset.id)
         dataset.save!
     end
@@ -95,7 +73,7 @@ class DatasetsController < ApplicationController
     private
 
     def dataset_params
-        permitted = params.require(:dataset).permit(:name,:description,:height,:width,:frames,:image_sequence,:ground_truth)
+        permitted = params.require(:dataset).permit(:name, :description, :image_sequence, :ground_truth)
         return permitted
     end
 end
--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,12 +2,66 @@ class Dataset < ActiveRecord::Base
     belongs_to :user
     validates :user_id, :name, :description, :image_sequence, :ground_truth, presence: true
     validate :proper_image_sequence_format
+
+    # Write an uploaded image sequence to the specified filename
+    def write_sequence_to_file(uploaded_file, filename)
+        FileUtils.mkdir_p(dir_path) unless File.directory?(dir_path)
+        file_path = Rails.root.join(dir_path, filename)
+        File.open(file_path, 'wb') do |file|
+            file.write(uploaded_file.read)
+        end
+    end
     
-    # Custom validations
+    # Find the dimensions of the image and update the dataset
+    def find_image_dimensions
+        file_path = Rails.root.join(dir_path, self.image_sequence)
+        File.readlines(file_path).each do |line|
+            if /DimSize =/ =~ line
+                self.width,self.height,self.frames = line.split(" ")[-3..-1]
+                break
+            end
+        end
+    end
+
+    # Create frame and thumbnail for .mha file
+    def create_frame_and_thumbnail
+        if (File.extname(self.image_sequence) == ".mha")
+            file_path = Rails.root.join(dir_path, self.image_sequence)
+            result = system(Rails.root.join('scripts','mha_to_png','bin',"MhaToPng #{file_path} 1").to_s)
+            base_name = File.basename(file_path, ".mha")
+            image = MiniMagick::Image.open("#{dir_path}/#{base_name}_frame.png")
+            image.resize "200x200"
+            image.format "png"
+            image.write "#{dir_path}/#{base_name}_thumbnail.png"
+            File.chmod(0644, "#{dir_path}/#{base_name}_thumbnail.png")
+            self.thumbnail = "#{base_name}_thumbnail.png"
+            self.frame = "#{base_name}_frame.png"
+        end
+    end
+
+    # Create a zip file containing the ground truth and image sequence
+    def create_zip
+        require 'zip'
+        zip_files = [self.ground_truth, self.image_sequence]
+        zip_filename = Rails.root.join(dir_path, "#{self.name}.zip")
+        Zip::File.open(zip_filename, Zip::File::CREATE) do |zipfile|
+            zip_files.each do |file|
+                zipfile.add(file, "#{dir_path}/#{file}")
+            end
+        end
+    end
+    
+    protected
+    
+    # Custom validation to ensure proper image format
     def proper_image_sequence_format
         if not image_sequence =~ /\.mha\z/
             errors.add(:image_sequence, "must be in .mha format currently")
         end
     end
-    
+
+    def dir_path
+        Rails.root.join('public', 'uploads', 'dataset', self.user.id.to_s, self.id.to_s)
+    end
+
 end
