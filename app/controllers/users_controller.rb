ec31720e72748238b78c94db5c36e8b1b440f227
Matt Lougheed <lougheed@cs.queensu.ca>
2015-11-11 11:17:32 -0500

    Added delete users option for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:destroy]
+    before_action :admin_user, only: :destroy
     
     def index
         @users = User.all
@@ -32,10 +34,20 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted"
+        redirect_to users_url
+    end
+
     private
 
     def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
     end
 
 end
